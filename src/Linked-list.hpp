68ec61b7dfdac5b15a58d3d0a8f7d74359b9707f
deGekata <y.zvonkov@yandex.ru>
2022-04-13 03:15:26 +0300

    fix gitignore

--- src/Linked-list.hpp
+++ src/Linked-list.hpp
@@ -18,7 +18,7 @@ typedef struct Entry {
 } Entry;
 
 
-void EntryCtor(Entry* ptr, HKey key, HValue value);//fffffffffff
+void EntryCtor(Entry* ptr, HKey key, HValue value); //fffffffffff
 void EntryCtor(Entry* ptr, Entry* ref);//fffffffffff
 
 void EntryLinkCtor(Entry* ptr, Entry* ref);
